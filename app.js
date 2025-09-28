98c23565e228d10383be4b920ebfa94387b1527d
Mike Denney <mike.a.denney@gmail.com>
2025-09-28 13:04:21 -0500

    append list function started

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 const editButton = document.getElementsByClassName('edit-item');
 const removeButton = document.getElementsByClassName('remove-item');
+const newListItem = document.getElementById('new-item');
 const shoppingList = document.getElementById('to-do-list');
 const addButton = document.getElementById('add-item');
 
+class ShoppingItem {
+    constructor()
+}
+
 addButton.addEventListener('click', function() {
-    //store value of input field
-    //kick off function to add to list
-    alert("Item added")
+    let inputText = newListItem.value;
+    appendList(inputText);
+    alert("Item added");
+    newListItem.value = '';
 });
 
+function appendList (listText) {
+    shoppingList.appendChild(document.createElement(
+        'li'))
+        
+}
 
-addItem.addEventListener('click', function(event) {
-    if(event.target.tagName === 'li') {
-        addItem.classList.add('list-item')
-        //need to define the HTML structure of the item we are 
-    }
-});
+// addButton.addEventListener('click', function(event) {
+//     if(event.target.tagName === 'li') {
+//         addButton.classList.add('list-item')
+//         //need to define the HTML structure of the item we are 
+//     }
+// });
 
 
