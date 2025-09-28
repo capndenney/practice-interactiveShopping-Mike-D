fabd8d3d809625afbfe80007bcb64af22460953d
Mike Denney <mike.a.denney@gmail.com>
2025-09-28 11:52:21 -0500

    add button started

---
+++ app.js
@@ -0,0 +1,20 @@
+const editButton = document.getElementsByClassName('edit-item');
+const removeButton = document.getElementsByClassName('remove-item');
+const shoppingList = document.getElementById('to-do-list');
+const addButton = document.getElementById('add-item');
+
+addButton.addEventListener('click', function() {
+    //store value of input field
+    //kick off function to add to list
+    alert("Item added")
+});
+
+
+addItem.addEventListener('click', function(event) {
+    if(event.target.tagName === 'li') {
+        addItem.classList.add('list-item')
+        //need to define the HTML structure of the item we are 
+    }
+});
+
+
